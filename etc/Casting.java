2fa322859aca50b15a0fcae2f8cda5b1740456a6
GYUHWAN SEOK <58979638+truespring@users.noreply.github.com>
2021-04-09 14:27:38 +0900

    형변환 업데이트

--- etc/Casting.java
+++ etc/Casting.java
@@ -1,21 +1,49 @@
 package prac;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Casting {
 
 	public static void main(String[] args) {
 
-//		int a = 100;
-//		double b = a;
-//		double c = 100;
-//		Double d = (double) a;
-//		Double e = b;
-//		Double f = c;
-//		Double g = 100.0;
-//		double h = g;
+		/**
+		 * double 은 기본 자료형, 일반 변수
+		 * Double 은 참조 자료형, 참조 변수
+		 */
 		
-		int a = -1;
-		Math.abs(a);
-		System.out.println(a);
+		int a = 100;
+		double b = a; // 자동 형변환
+		double c = 100; // 자동 형변환
+		Double d = (double) a; // 자동 형변환 안됨
+		Double e = b;
+		Double f = c;
+		Double g = 100.0;
+		double h = g;
+		
+//		List<Double> doubleArr = new ArrayList();
+//		doubleArr.add(a);
+//		doubleArr.add(b);
+//		doubleArr.add(c);
+//		doubleArr.add(d);
+//		doubleArr.add(e);
+//		doubleArr.add(f);
+//		doubleArr.add(g);
+//		doubleArr.add(h);
+//
+//		List<double> doubleArr_2 = new ArrayList();
+//		doubleArr_2.add(a);
+//		doubleArr_2.add(b);
+//		doubleArr_2.add(c);
+//		doubleArr_2.add(d);
+//		doubleArr_2.add(e);
+//		doubleArr_2.add(f);
+//		doubleArr_2.add(g);
+//		doubleArr_2.add(h);
+		
+//		int a = -1;
+//		Math.abs(a);
+//		System.out.println(a);
 	}
 
 }
