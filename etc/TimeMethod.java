f9de1439b2b6a736469116b792ad821bb3ba269d
GYUHWAN SEOK <58979638+truespring@users.noreply.github.com>
2021-04-16 16:26:34 +0900

    시간 관련 클래스와 메소드 정리

---
+++ etc/TimeMethod.java
@@ -0,0 +1,33 @@
+package com.doosan.test.prac;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TimeMethod {
+
+	public static void main(String[] args) {
+		long nowTime = System.currentTimeMillis();
+		System.out.println(nowTime); // 1618557846668
+		
+		Date currentDate = new Date();
+		System.out.println(currentDate); // Fri Apr 16 16:24:06 KST 2021
+		System.out.println(dateToTimeString(currentDate)); // 20210416162406
+		
+	}
+	
+	public static Long dateToTimeString(Date date) {
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+		String dateString = simpleDateFormat.format(date);
+		return objectToLong(dateString);
+	}
+	
+	public static Long objectToLong(Object object) {
+		Long longObject = null;
+		try {
+			longObject = Long.parseLong(object.toString());
+		} catch (NumberFormatException | NullPointerException e) {
+			
+		}
+		return longObject;
+	}
+}
