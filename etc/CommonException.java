38819b4c1e5f45131645b911bc8a2801b9414a80
GYUHWAN SEOK <58979638+truespring@users.noreply.github.com>
2021-04-27 09:44:23 +0900

    CommonException 생성과 활용법 정리

--- etc/CommonException.java
+++ etc/CommonException.java
@@ -1,36 +1,26 @@
-package com.doosan.test.prac;
+package com.doosan.test.prac2;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CommonException extends Exception {
-	/**
-	 * 링크 : https://mvnrepository.com/artifact/org.slf4j/slf4j-api/1.7.30
-	 * 링크에서 외부라이브러리를 받아야 함 
-	 */
 	private static final long serialVersionUID = -6124459025372348072L;
-
+	
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	private final String defaultErrorMessage = "Reason not specified.";
-
+	
 	public CommonException() {
 		super();
-
 		log.error(defaultErrorMessage);
 	}
-
+	
 	public CommonException(String message) {
 		super(message);
 		log.error(message);
 	}
-
+	
 	public CommonException(String message, Throwable cause) {
 		super(message, cause);
 		log.error(message, cause);
-	}
-
-	public CommonException(Throwable cause) {
-		super(cause);
-		log.error(defaultErrorMessage, cause);
 	}
 }
--- etc/ExceptionTest.java
+++ etc/ExceptionTest.java
@@ -1,14 +1,24 @@
-package com.doosan.test.prac;
+package com.doosan.test.prac2;
 
 public class ExceptionTest {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws CommonException {
+		/**
+		 * 링크 : https://www.nextree.co.kr/p3239/
+		 * 오류(Error) : 시스템에 비정상적인 상황이 생겼을 때 발생한다. 
+		 * 				이는 시스템 레벨에서 발생하기 때문에 심각한 수준의 오류이다.
+		 * 				개발자가 미리 예측하여 처리할 수 없기 때문에, 애플리케이션에서 오류에 대한 처리를 신경 쓰지 않아도 된다
+		 * 예외(Exception) : 개발자가 구현한 로직에서 발생항다. 예외는 발생할 상황을 미리 예측하여 처리할 수 있다.
+		 */
+		String str = null;
+		ExceptionTest et = new ExceptionTest();
+		et.test(str);
 	}
 	
-	public void testMethod(String a) throws CommonException {
-		if(a == null) {
-			throw new CommonException("error code");
+	public String test(String str) throws CommonException {
+		if(str == null) {
+			throw new CommonException("Null!");
 		}
+		return str;
 	}
 }
