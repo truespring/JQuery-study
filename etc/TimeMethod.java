f36e793f01a2d4f0f3fa5e83b08c4dde13f168d1
GYUHWAN SEOK <58979638+truespring@users.noreply.github.com>
2021-04-27 16:32:55 +0900

    내용 및 주석 추가

--- etc/TimeMethod.java
+++ etc/TimeMethod.java
@@ -1,12 +1,13 @@
 package com.doosan.test.prac;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class TimeMethod {
 
 	public static void main(String[] args) {
-		long nowTime = System.currentTimeMillis();
+		Long nowTime = System.currentTimeMillis();
 		System.out.println(nowTime); // 1618557846668
 		
 		Date currentDate = new Date();
@@ -17,14 +18,20 @@ public class TimeMethod {
 	
 	public static Long dateToTimeString(Date date) {
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
-		String dateString = simpleDateFormat.format(date);
+		String dateString = simpleDateFormat.format(date); // Date -> String 형변환
+		try {
+			date = simpleDateFormat.parse(dateString); // String -> Date 형변환
+			System.out.println(date); // Fri Apr 16 16:24:06 KST 2021
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 		return objectToLong(dateString);
 	}
 	
 	public static Long objectToLong(Object object) {
 		Long longObject = null;
 		try {
-			longObject = Long.parseLong(object.toString());
+			longObject = Long.parseLong(object.toString()); // Object -> Long 형변환
 		} catch (NumberFormatException | NullPointerException e) {
 			
 		}
