657586942d9eae5daa551e60100f28eb423a8dc0
GYUHWAN SEOK <58979638+truespring@users.noreply.github.com>
2021-05-07 10:10:10 +0900

    Exception 테스트

--- etc/ExceptionTest.java
+++ etc/ExceptionTest.java
@@ -1,24 +1,33 @@
-package com.doosan.test.prac2;
+package com.doosan.test.prac;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ExceptionTest {
+	private final Logger logger = LoggerFactory.getLogger(ExceptionTest.class);
 
-	public static void main(String[] args) throws CommonException {
-		/**
-		 * 링크 : https://www.nextree.co.kr/p3239/
-		 * 오류(Error) : 시스템에 비정상적인 상황이 생겼을 때 발생한다. 
-		 * 				이는 시스템 레벨에서 발생하기 때문에 심각한 수준의 오류이다.
-		 * 				개발자가 미리 예측하여 처리할 수 없기 때문에, 애플리케이션에서 오류에 대한 처리를 신경 쓰지 않아도 된다
-		 * 예외(Exception) : 개발자가 구현한 로직에서 발생항다. 예외는 발생할 상황을 미리 예측하여 처리할 수 있다.
-		 */
-		String str = null;
+	public static void main(String[] args) {
 		ExceptionTest et = new ExceptionTest();
-		et.test(str);
+		try {
+			String a = null;
+			et.testMethod(a);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public String test(String str) throws CommonException {
-		if(str == null) {
-			throw new CommonException("Null!");
+	public void testMethod(String a) throws Exception {
+//		if(a == null) {
+//			throw new CommonException("무~야호~");
+//		}
+		try {
+			String b = (String) a;
+			Long c = Long.parseLong(b);
+//			System.out.println(a.toString());
+			System.out.println(b);
+			System.out.println(c);
+		} catch(Exception e) {
+			throw new CommonException("무~야호~");
 		}
-		return str;
 	}
 }
