fc4f8651c0ed89273aa344eb36cb614acca4bd22
GYUHWAN SEOK <58979638+truespring@users.noreply.github.com>
2021-05-12 09:20:29 +0900

    sort 내용 추가

--- etc/SortTest.java
+++ etc/SortTest.java
@@ -1,7 +1,7 @@
 package com.doosan.test.prac3;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -11,12 +11,19 @@ public class SortTest {
 
 	public static void main(String[] args) {
 		List<SampleVO> voList = new ArrayList<>();
-		Map<String, Object> map = new HashMap<>();
+		Map<String, Object> map = new LinkedHashMap<>();
 		map.put("distance", "33");
-		Map<String, Object> map2 = new HashMap<>();
+		map.put("distance4", "4");
+		map.put("distance0", "1");
+		Map<String, Object> map2 = new LinkedHashMap<>();
 		map2.put("distance", "23");
-		Map<String, Object> map3 = new HashMap<>();
+		map2.put("distance2", "55");
+		map2.put("distance3", "2");
+		Map<String, Object> map3 = new LinkedHashMap<>();
 		map3.put("distance", "32");
+		map3.put("distance2", "1");
+		map3.put("distance3", "3");
+//		System.out.println(map.get("distance").getClass().getName());
 		SampleVO vo = new SampleVO("AA", 1, "b", map);
 		SampleVO vo2 = new SampleVO("CC", 5, "F", map2);
 		SampleVO vo3 = new SampleVO("B", 2, "1", map3);
@@ -31,15 +38,47 @@ public class SortTest {
 		for(SampleVO a : voList) {
 			System.out.println(a.toString());
 		}
+		/*
+		 *  AA 1 b {distance=33, distance4=4, distance0=1}
+			CC 5 F {distance=23, distance2=55, distance3=2}
+			B 2 1 {distance=32, distance2=1, distance3=3}
+		 */
 		System.out.println("===========");
-		
-		voList.sort((o1, o2) -> ((String) o1.getMap().get("distance")).compareTo((String)o2.getMap().get("distance"))); // 문자열 오름차순정렬
+
 		voList.stream().map(item -> item.getMap().get("distance")).forEach(System.out::println);
-//		System.out.println("===========");
-//		
+		/*
+		 *  33
+			23
+			32
+		 */
+		System.out.println("===========");
+		voList.sort((o1, o2) -> ((String) o1.getMap().get("distance")).compareTo((String)o2.getMap().get("distance"))); // 문자열 오름차순정렬
+		voList.stream().map(item -> item.toString()).forEach(System.out::println);
+		/*
+		 *  CC 5 F {distance=23, distance2=55, distance3=2}
+			B 2 1 {distance=32, distance2=1, distance3=3}
+			AA 1 b {distance=33, distance4=4, distance0=1}
+		 */
+		System.out.println("===========");
+		voList.stream().map(item -> item.getMap().get("distance")).forEach(System.out::println);
+		/*
+		 *  23
+			32
+			33
+		 */
+
+		
 //		voList.sort((o1, o2) -> Long.compare(o1.getNum(), o2.getNum())); // 숫자형 오름차순정렬
 //		for(SampleVO a : voList) {
 //			System.out.println(a.toString());
 //		}
+		
+		Map<String, Object> map4 = new LinkedHashMap<>();
+		map4.put("distance1", "1");
+		map4.put("distance4", "1");
+		map4.put("distance3", "1");
+		map4.put("distance6", "1");
+		map4.put("distance2", "1");
+		System.out.println(map4);
 	}
 }
