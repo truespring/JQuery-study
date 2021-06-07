572ffb462f95730279b4ea03edc47589aba31647
GYUHWAN SEOK <58979638+truespring@users.noreply.github.com>
2021-06-07 15:10:04 +0900

    DoubleSummaryStatistics 예제

--- etc/StreamForeach.java
+++ etc/StreamForeach.java
@@ -1,6 +1,7 @@
 package com.doosan.test.prac3;
 
 import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
 
 import com.doosan.test.SampleVO;
@@ -13,12 +14,17 @@ public class StreamForeach {
 		objArray.add(objList);
 		
 		List<SampleVO> voList = new ArrayList<>();
-		objArray.stream().forEach(item -> {
+		objArray.forEach(item -> {
 			Object[] data = item;
 			SampleVO vo = new SampleVO();
 			vo.setS(data[0].toString());
 			vo.setStr(data[1].toString());
+			vo.setHeight(123D);
 			voList.add(vo);
 		});
+		
+		DoubleSummaryStatistics predictValueStatistics = voList.stream().filter(item -> item != null && item.getHeight() != null).mapToDouble(SampleVO::getHeight).summaryStatistics();
+		// DoubleSummaryStatistics{count=1, sum=123.000000, min=123.000000, average=123.000000, max=123.000000}
+		System.out.println(predictValueStatistics);
 	}
 }
