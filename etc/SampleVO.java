680d1d982c9a7045ccab987d1f190cce75e13769
GYUHWAN SEOK <58979638+truespring@users.noreply.github.com>
2021-06-08 10:24:22 +0900

    DoubleSummaryStatistics 테스트

--- etc/SampleVO.java
+++ etc/SampleVO.java
@@ -1,10 +1,48 @@
 package com.doosan.test;
 
+import java.util.Map;
+
 public class SampleVO {
 
 	private String s;
 	private int num;
 	private String str;
+	private Double height;
+	private Map<String, Object> map;
+	
+	public Double getHeight() {
+		return height;
+	}
+
+	public void setHeight(Double height) {
+		this.height = height;
+	}
+
+	public Map<String, Object> getMap() {
+		return map;
+	}
+
+	public void setMap(Map<String, Object> map) {
+		this.map = map;
+	}
+
+	public SampleVO() {
+		super();
+	}
+
+	public SampleVO(String s, int num, String str) {
+		super();
+		this.s = s;
+		this.num = num;
+	}
+		
+	public SampleVO(String s, int num, String str, Map<String, Object> map) {
+		super();
+		this.s = s;
+		this.num = num;
+		this.str = str;
+		this.map = map;
+	}
 
 	public String getStr() {
 		return str;
@@ -28,5 +66,10 @@ public class SampleVO {
 
 	public void setS(String s) {
 		this.s = s;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("{s=%s, num=%d, str=%s, map=%s}", this.s, this.num, this.str, this.map);
 	}
 }
--- etc/StreamForeach.java
+++ etc/StreamForeach.java
@@ -10,8 +10,10 @@ public class StreamForeach {
 
 	public static void main(String[] args) {
 		List<Object[]> objArray = new ArrayList<>();
-		Object[] objList = new Object[] {"aaa", "bbb"};
+		Object[] objList = new Object[] {"aaa", "bbb", 5D};
+		Object[] objList2 = new Object[] {"aaa", "bbb", 3D};
 		objArray.add(objList);
+		objArray.add(objList2);
 		
 		List<SampleVO> voList = new ArrayList<>();
 		objArray.forEach(item -> {
@@ -19,12 +21,21 @@ public class StreamForeach {
 			SampleVO vo = new SampleVO();
 			vo.setS(data[0].toString());
 			vo.setStr(data[1].toString());
-			vo.setHeight(123D);
+			vo.setHeight(Double.parseDouble(data[2].toString()));
+			System.out.println(vo.toString());
 			voList.add(vo);
 		});
 		
 		DoubleSummaryStatistics predictValueStatistics = voList.stream().filter(item -> item != null && item.getHeight() != null).mapToDouble(SampleVO::getHeight).summaryStatistics();
-		// DoubleSummaryStatistics{count=1, sum=123.000000, min=123.000000, average=123.000000, max=123.000000}
+		// DoubleSummaryStatistics{count=2, sum=8.000000, min=3.000000, average=4.000000, max=5.000000}
 		System.out.println(predictValueStatistics);
+		
+		Integer a = 1;
+		Long b = 1L;
+		Double c = 1D;
+		a++;
+		b++;
+		c++;
+		System.out.println(a + " " + b + " " + c);
 	}
 }
