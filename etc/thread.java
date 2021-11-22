93785e75b988797e8da60c91273c418a00d5752f
Gyuhwan Seok <truespring1@gmail.com>
2021-11-22 16:30:20 +0900

    thread 관련 내용 추가

---
+++ etc/thread.java
@@ -0,0 +1,20 @@
+class MultiThread extends Thread {
+
+    String name;
+    int id;
+
+    MultiThreadSelectInsert(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+
+    public void run() {
+
+        try {
+            // thread 설정
+            // 로직 구현
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
